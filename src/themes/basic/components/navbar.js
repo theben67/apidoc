871cc906e5044d4c6416631b4206a81cf77a2265
Benjamin <benjamin.h.67110@gmail.com>
2022-06-08 16:37:22 +0200

    Update navbar.js

--- src/themes/basic/components/navbar.js
+++ src/themes/basic/components/navbar.js
@@ -2,7 +2,7 @@ module.exports = function (datas){
   return `
     <div class="navbarContainer">
         ${datas.root_files.map(x => `
-            <a href="{{{url}}}${x.toLowerCase()}/index.html"> ${x.split("_").join(" ")} </a>
+            <a href="{{{url}}}${x.toLowerCase()}"> ${x.split("_").join(" ")} </a>
           `).join("")}
         <a href="{{{url}}}index.html"><img src="{{{url}}}logo.png" /></a>
     </div>
